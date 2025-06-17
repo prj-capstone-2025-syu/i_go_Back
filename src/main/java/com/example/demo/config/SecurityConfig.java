2445c7cb4c8d23a4014e36ac0c424c1f64241f31
kod0406 <kod04061@gmail.com>
2025-06-17 12:33:30 +0900

    수정

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -40,8 +40,8 @@ public class SecurityConfig {
     }
 
     //TODO : 나중에 EC2 옮길떄 바꿀것
-    //@Value("${frontend.url:https://www.igo.ai.kr}")
-    @Value("${frontend.url:http://localhost:3000}")
+    @Value("${frontend.url:https://www.igo.ai.kr}")
+    //@Value("${frontend.url:http://localhost:3000}")
     private String frontendUrl;
 
     @Bean
