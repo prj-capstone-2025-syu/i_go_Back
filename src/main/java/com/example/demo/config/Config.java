80db1d2518d5bdf7c4ffd4ea03c5a3c0d945d56a
kod0406 <kod04061@gmail.com>
2025-08-13 17:25:14 +0900

    Bean 중복정의 해결

--- src/main/java/com/example/demo/config/Config.java
+++ src/main/java/com/example/demo/config/Config.java
@@ -4,11 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import jakarta.annotation.PostConstruct;
-import java.time.ZoneId;
 import java.util.TimeZone;
 
 @Configuration
@@ -18,11 +15,6 @@ public class Config {
     public void setTimezone() {
         // 애플리케이션 전체 타임존을 한국 시간으로 설정
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean
